7b5ce69abfc79f2630f9b9fc9aa56e90c7065927
tscherkas <tscherkas.it@gmail.com>
2016-02-18 02:26:34 +0300

    Added sql-data-scripts

---
+++ WebApplication/DatabaseProject/dbo/Tables/dbo.Jobs.data.sql
@@ -0,0 +1,5 @@
+﻿SET IDENTITY_INSERT [dbo].[Jobs] ON
+INSERT INTO [dbo].[Jobs] ([Job_id], [Job_nm]) VALUES (1, N'Developer')
+INSERT INTO [dbo].[Jobs] ([Job_id], [Job_nm]) VALUES (2, N'QA-engineer')
+INSERT INTO [dbo].[Jobs] ([Job_id], [Job_nm]) VALUES (3, N'Manager')
+SET IDENTITY_INSERT [dbo].[Jobs] OFF
